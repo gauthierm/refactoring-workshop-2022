7d9383bf7c1aec5c0f7cc6cd9dd672352f9ce01c
Michael Gauthier <mike@silverorange.com>
2022-11-24 22:10:14 -0400

    Step 7. Use a functional approach to getGroupedMemberData

--- src/html/getGroupedMemberData.ts
+++ src/html/getGroupedMemberData.ts
@@ -1,16 +1,28 @@
 export function getGroupedMemberData(contributors: any[], members: any[]) {
-  const groupedMembers: { [key: string]: any[] } = { facebook: [], other: [] };
-  const totals: { [key: string]: number } = { facebook: 0, other: 0 };
+  const groupedMembers = contributors.reduce<{
+    facebook: any[];
+    other: any[];
+  }>(
+    ({ facebook, other }, contributor) => {
+      return members.includes(contributor.login)
+        ? { facebook: [...facebook, contributor], other }
+        : { facebook, other: [...other, contributor] };
+    },
+    { facebook: [], other: [] }
+  );
 
-  contributors.forEach((contributor) => {
-    if (members.includes(contributor.login)) {
-      groupedMembers.facebook.push(contributor);
-      totals.facebook += contributor.contributions;
-    } else {
-      groupedMembers.other.push(contributor);
-      totals.other += contributor.contributions;
-    }
-  });
+  const totals = Object.fromEntries(
+    Object.entries(groupedMembers).map(([group, contributors]) => {
+      return [
+        group,
+        contributors.reduce(
+          (total: number, contributor: any) =>
+            total + contributor.contributions,
+          0
+        ),
+      ];
+    })
+  );
 
   groupedMembers.facebook.sort((a, b) => b.contributions - a.contributions);
   groupedMembers.other.sort((a, b) => b.contributions - a.contributions);
