8ca68e9e66691bb0974c2924bc6bc2ddb0bb6c0a
Michael Gauthier <mike@silverorange.com>
2022-11-24 22:56:51 -0400

    Make slow middleware faster

--- src/middleware/slow.ts
+++ src/middleware/slow.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 const MIN_DELAY_MS = 20;
-const MAX_DELAY_MS = 200;
+const MAX_DELAY_MS = 50;
 
 export function slow(req: Request, res: Response, next: NextFunction) {
   const delay = MIN_DELAY_MS + Math.random() * (MAX_DELAY_MS - MIN_DELAY_MS);
